708e3c618a962d694a8bfdbc5b1ef66364f55253
23130288 <23130288@st.hcmuaf.edu.vn>
2026-06-08 21:39:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project/StartingMenuActivity.java
@@ -0,0 +1,32 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StartingMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.starting_menu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnPlay = findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(StartingMenuActivity.this, ModeMenuActivity.class);
+            startActivity(intent);
+        });
+    }
+}
