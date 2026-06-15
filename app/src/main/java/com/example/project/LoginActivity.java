c47a1cb19c9516cc325d6da69c800fee74c2f7c7
tantai170804 <22130239@st.hcmafu.edu.vn>
2026-06-15 20:24:23 +0700

    thay thế tk cố định = xác thực firebase thật

--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import Model.Session;
 
@@ -16,30 +21,63 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtPassword;
     private Button btnLogin;
 
+    private TextView tvForgotPassword;
+    private TextView tvRegister;
+    private FirebaseAuth mAuth;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+        tvRegister = findViewById(R.id.tvRegister);
+
 
         btnLogin.setOnClickListener(v -> {
 
             String email = edtEmail.getText().toString().trim();
             String password = edtPassword.getText().toString().trim();
 
-            if(email.equals("admin@gmail.com") && password.equals("123456")) {
-                Session.isLoggedIn = true;
-                Session.email = email;
-                Toast.makeText(  this,   "Đăng nhập thành công",Toast.LENGTH_SHORT).show();
-                finish();
+            if(TextUtils.isEmpty(password)) {
+                edtEmail.setError("Vui lòng nhập email");
+                return;
             }
-            else {
-                Toast.makeText(this,"Sai email hoặc mật khẩu",Toast.LENGTH_SHORT).show();
+            if(TextUtils.isEmpty(password)) {
+                edtPassword.setError("Vui lòng nhập mật khẩu");
+                return;
             }
+            mAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this,task -> {
+                        if (task.isSuccessful()) {
+                            Session.isLoggedIn = true;
+                            Session.email = email;
+                            Toast.makeText(  this,   "Đăng nhập thành công",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this,StartingMenuActivity.class);
+                            startActivity(intent);
+                            finish();
+            }else {
+                   String errorMsg = task.getException() != null ? task.getException().getMessage() : "Sai email hoặc mật khẩu";
+                            Toast.makeText(this, "Đăng nhập thất bại: " + errorMsg, Toast.LENGTH_LONG).show();
+
+                        }
+            });
         });
+
+        tvForgotPassword.setOnClickListener(view ->
+                Toast.makeText(this,"Chức năng Quên mật khẩu đang được xử lý" , Toast.LENGTH_SHORT).show()
+                );
+        tvRegister.setOnClickListener(view ->
+                Toast.makeText(this,"Chức năng Đăng ký đang được xử lý" , Toast.LENGTH_SHORT).show()
+                );
     }
 }
